fe526b6b9f0acad471c9128424046560e27ae44c
RaoFaizanAliAhmed <raofaizanaliahmed@gmail.com>
2024-09-14 04:08:13 +0500

    Add files via upload

---
+++ chap 5/index5.js
@@ -0,0 +1,107 @@
+//Question 1 //
+var number1 = 6;
+ var number2 = 9;
+
+            
+            var plus = number1 + number2;
+
+            
+            var result = "Sum of " + number1 + " and " + number2 + " is " + plus;
+
+            
+            document.write(result);
+            //Question 1 //
+            //.....//
+
+            //Question 2//
+            var number1 = 6;
+ var number2 = 9;
+
+            
+            var minus = number1 - number2;
+
+            var number1 = 6;
+ var number2 = 9;
+
+            
+            var subs = number1 * number2;
+
+            var number1 = 6;
+ var number2 = 9;
+
+            
+            var divi = number1 / number2;
+
+            var b = "minus of " + number1 + " and " - number2 + " is " + minus;
+
+            
+            document.write(b);
+
+            var c = "subs of " + number1 + " and " * number2 + " is " + subs;
+
+            
+            document.write(c);
+
+            var d = "divi of " + number1 + " and " / number2 + " is " + divi;
+
+            
+            document.write(d);
+
+
+            //Question no 3//
+
+
+             // a. Declare a variable.
+        var myVar;
+
+        // b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+        document.write("Value after variable declaration is: " + myVar + "<br>");
+
+        // c. Initialize the variable with some number.
+        myVar = 5;
+
+        // d. Show the value of variable in your browser like “Initial value: 5”.
+        document.write("Initial value: " + myVar + "<br>");
+
+        // e. Increment the variable.
+        myVar++;
+
+        // f. Show the value of variable in your browser like “Value after increment is: 6”.
+        document.write("Value after increment is: " + myVar + "<br>");
+
+        // g. Add 7 to the variable.
+        myVar += 7;
+
+        // h. Show the value of variable in your browser like “Value after addition is: 13”.
+        document.write("Value after addition is: " + myVar + "<br>");
+
+        // i. Decrement the variable.
+        myVar--;
+
+        // j. Show the value of variable in your browser like “Value after decrement is: 12”.
+        document.write("Value after decrement is: " + myVar + "<br>");
+
+        // k. Show the remainder after dividing the variable’s value by 3.
+        var remainder = myVar % 3;
+
+        // l. Output : “The remainder is : 0”.
+        document.write("The remainder is: " + remainder);
+
+
+
+
+
+           //Question no 4//
+
+
+ var ticketPrice = 600;
+
+
+ var numberOfTickets = 5;
+ var totalCost = ticketPrice * numberOfTickets;
+
+ document.getElementById("ticket-cost").innerHTML = 
+     "The cost of buying " + numberOfTickets + " movie tickets is " + totalCost + " PKR.";
+
+
+ 
