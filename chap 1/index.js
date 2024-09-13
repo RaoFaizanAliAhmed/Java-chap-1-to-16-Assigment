c4537dc945567a7b47735fb4d5a15821a995295a
RaoFaizanAliAhmed <raofaizanaliahmed@gmail.com>
2024-09-14 04:06:10 +0500

    Add files via upload

---
+++ chap 1/index.js
@@ -0,0 +1,54 @@
+//Question 1
+
+var greeting = "Welcome to our website!";
+        alert(greeting);
+
+        //Question 1 
+        //.......//
+
+
+
+
+        //Question 2//
+
+        var message = "Error! please enter a valid password<br/>";
+        document.write(message);
+
+        //Question 2//
+        //......//
+ 
+
+
+        //Question 3//
+
+        var welcome = "<br/>Welcome to JS land... <br/>Happy coding!"
+        document.write(welcome)
+
+        //Question 3 //
+        //.......//
+
+
+
+
+        //Question 4 //
+        var welcome = "Welcome to JS land..."
+        alert(welcome)
+
+        var second = "Happy coding!\nPrevent this page from creating additional dialogs."
+        alert(second)
+
+ //Question 4 //
+ //......//
+
+
+
+ //Question 5 // 
+console.log("Hello...I can run JS through my web browsers console")
+
+//Question 5 //
+//.......//
+
+
+
+
+//Question 6 //
